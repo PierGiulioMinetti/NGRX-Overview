6516e378c225defada727b063803f088614976f6
PierGiulioMinetti <pg.minetti@gmail.com>
2023-05-27 16:17:57 +0200

    todos + customers created + routing created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
-
+const routes: Routes = [
+  { path: 'customers', loadChildren: () => import('./customers/customers.module').then(m => m.CustomersModule) }, 
+  { path: 'todos', loadChildren: () => import('./todos/todos.module').then(m => m.TodosModule) }];
+ 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
