58b4e3d8eb6eab05d880456008e37cdd768a30d3
PierGiulioMinetti <pg.minetti@gmail.com>
2023-05-31 15:50:45 +0200

    customer action added load + json server installed

--- src/app/customers/store/customers.action.ts
+++ src/app/customers/store/customers.action.ts
@@ -2,3 +2,9 @@ import { createAction, props } from "@ngrx/store";
 import { Customer } from "./customers.model";
 
 export const addCustomer = createAction('Customer Page], Add Customer', props<{customer:Customer}>());
+
+export const loadCustomer = createAction('[Customer Page], Load Customer');
+
+export const loadCustomerSuccess = createAction('[Customer API], Load Customer Success', props<{customer:Customer[]}>);
+
+export const loadCustomerFailure = createAction('[Customer API], Load Customer Failure', props<{error:string}>);
